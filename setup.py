585ab469708084161010484a97812020f92e9933
mikkel32 <120693567+mikkel32@users.noreply.github.com>
2025-08-08 06:41:10 -0700

    Run overlay.choose on UI thread

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "1.3.51"
+__version__ = "1.3.52"
 
 import argparse
 import os
--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 """Public package interface for CoolBox."""
 
-__version__ = "1.3.51"
+__version__ = "1.3.52"
 
 import os
 
